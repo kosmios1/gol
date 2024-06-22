d2db804d9855904299d0b3e6038f9778dc3a7b92
cocsx <82763502+cocsx@users.noreply.github.com>
2024-06-22 15:19:22 +0200

    feat(core): refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,108 @@
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::{event::Event, render::Canvas};
+
+use std::collections::HashMap;
 use std::thread::sleep;
 use std::time::Duration;
-use std::usize;
 
-const GRID_WIDTH: i64 = 100;
-const GRID_HEIGHT: i64 = 25;
+// const GRID_WIDTH: i64 = 30;
+// const GRID_HEIGHT: i64 = 30;
 
 const GAP: u32 = 10;
 const WINDOW_WIDTH: u32 = 960;
 const ASPECT_RATIO: f32 = 9.0 / 16.0;
 
+// Oscillator
+// grid[idx2dto1d(10, 10)] = CELL::Alive;
+// grid[idx2dto1d(11, 10)] = CELL::Alive;
+// grid[idx2dto1d(12, 10)] = CELL::Alive;
+// grid[idx2dto1d(11, 11)] = CELL::Alive;
+// grid[idx2dto1d(12, 11)] = CELL::Alive;
+// grid[idx2dto1d(13, 11)] = CELL::Alive;
+
+// Glider
+// grid[idx2dto1d(1, 1)] = CELL::Alive;
+// grid[idx2dto1d(2, 2)] = CELL::Alive;
+// grid[idx2dto1d(2, 3)] = CELL::Alive;
+// grid[idx2dto1d(1, 3)] = CELL::Alive;
+// grid[idx2dto1d(0, 3)] = CELL::Alive;
+
+// Penta-decathlon
+// grid[idx2dto1d(15, 0)] = CELL::Alive;
+// grid[idx2dto1d(15, 1)] = CELL::Alive;
+// grid[idx2dto1d(17, 0)] = CELL::Alive;
+// grid[idx2dto1d(16, 1)] = CELL::Alive;
+// grid[idx2dto1d(16, 2)] = CELL::Alive;
+
+// grid[idx2dto1d(22, 2)] = CELL::Alive;
+// grid[idx2dto1d(22, 3)] = CELL::Alive;
+// grid[idx2dto1d(22, 4)] = CELL::Alive;
+// grid[idx2dto1d(23, 4)] = CELL::Alive;
+// grid[idx2dto1d(24, 3)] = CELL::Alive;
+
+// grid[idx2dto1d(7, 15)] = CELL::Alive;
+// grid[idx2dto1d(8, 15)] = CELL::Alive;
+// grid[idx2dto1d(9, 15)] = CELL::Alive;
+// grid[idx2dto1d(9, 16)] = CELL::Alive;
+// grid[idx2dto1d(8, 17)] = CELL::Alive;
+
 #[derive(Clone, PartialEq)]
-enum CELL {
+enum CellType {
 	Alive,
 	Dead,
 }
 
-fn idx2dto1d(x: i64, y: i64) -> usize {
-	return (x + GRID_WIDTH * y) as usize;
+struct Cell {
+	pos: (i64, i64),
+	cell_type: CellType,
 }
 
-fn get_neigh_count(grid: &Vec<CELL>, x: i64, y: i64) -> i64 {
-	static OFFSETS: [(i64, i64); 8] =
-		[(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
-
-	let mut alive_neigh = 0;
-	for p in &OFFSETS {
-		let mut nx = p.0 + x;
-		let mut ny = p.1 + y;
-
-		if nx < 0 || nx > GRID_WIDTH {
-			nx = nx.rem_euclid(GRID_WIDTH);
-		}
-
-		if ny < 0 || ny > GRID_HEIGHT {
-			ny = ny.rem_euclid(GRID_HEIGHT);
-		}
-
-		if idx2dto1d(nx, ny) >= 2500 {
-			continue;
-		}
-
-		match grid[idx2dto1d(nx, ny) as usize] {
-			CELL::Dead => alive_neigh += 0,
-			CELL::Alive => alive_neigh += 1,
-		}
+impl Cell {
+	fn new(pos: (i64, i64), cell_type: CellType) -> Cell {
+		Cell { pos, cell_type }
 	}
-	return alive_neigh;
 }
 
-fn game_cyle(grid: &Vec<CELL>, canvas: &mut Canvas<sdl2::video::Window>) -> Vec<CELL> {
-	let quad_width: u32 = (WINDOW_WIDTH / GRID_WIDTH as u32) as u32;
-	let quad_height: u32 =
-		((WINDOW_WIDTH as f32 * ASPECT_RATIO) as u32 / GRID_HEIGHT as u32) as u32;
+struct Grid {
+	grid: HashMap<(i64, i64), Cell>,
+}
 
-	let mut new_grid: Vec<CELL> = vec![CELL::Dead; (GRID_WIDTH * GRID_HEIGHT) as usize];
-
-	for y in 0..GRID_HEIGHT {
-		for x in 0..GRID_WIDTH {
-			let neigh = get_neigh_count(&grid, x, y);
-			match grid[idx2dto1d(x, y)] {
-				CELL::Alive => {
-					if neigh < 2 {
-						new_grid[idx2dto1d(x, y)] = CELL::Dead;
-					} else if neigh == 2 || neigh == 3 {
-						new_grid[idx2dto1d(x, y)] = CELL::Alive;
-					} else if neigh >= 3 {
-						new_grid[idx2dto1d(x, y)] = CELL::Dead;
-					}
-				}
-				CELL::Dead => {
-					if neigh == 3 {
-						new_grid[idx2dto1d(x, y)] = CELL::Alive;
-					}
-				}
-			}
-
-			if new_grid[idx2dto1d(x, y)] == CELL::Alive {
-				canvas.set_draw_color(Color::RGB(255, 255, 255));
-			} else {
-				canvas.set_draw_color(Color::RGB(0, 0, 0));
-			}
-			let rectangle = sdl2::rect::Rect::new(
-				((x as i32) * quad_width as i32) + (GAP as i32 * 3) as i32,
-				((y as i32) * quad_height as i32) + (GAP as i32 * 3) as i32,
-				quad_width,
-				quad_height,
-			);
-			let _ = canvas.fill_rect(rectangle);
+impl Grid {
+	pub fn new() -> Grid {
+		Grid {
+			grid: HashMap::new(),
 		}
 	}
-	return new_grid;
+
+	pub fn add_cell(&mut self, pos: (i64, i64), typ: CellType) {
+		self.grid.insert(pos, Cell::new(pos, typ));
+	}
+
+	fn get_neigh_count(&self, x: i64, y: i64) -> i64 {
+		static OFFSETS: [(i64, i64); 8] =
+			[(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+
+		let mut alive_neigh = 0;
+		for p in &OFFSETS {
+			let nx = match p.0.checked_add(x) {
+				Some(px) => px,
+				None => 0,
+			};
+			let ny = match p.1.checked_add(y) {
+				Some(py) => py,
+				None => 0,
+			};
+
+			if self.grid.contains_key(&(nx, ny)) {
+				alive_neigh += 1;
+			}
+		}
+		return alive_neigh;
+	}
 }
 
 fn main() {
-	let mut grid: Vec<CELL> = vec![CELL::Dead; (GRID_WIDTH * GRID_HEIGHT) as usize];
-
-	// Oscillator
-	grid[idx2dto1d(10, 10)] = CELL::Alive;
-	grid[idx2dto1d(11, 10)] = CELL::Alive;
-	grid[idx2dto1d(12, 10)] = CELL::Alive;
-	grid[idx2dto1d(11, 11)] = CELL::Alive;
-	grid[idx2dto1d(12, 11)] = CELL::Alive;
-	grid[idx2dto1d(13, 11)] = CELL::Alive;
-
-	// Glider
-	grid[idx2dto1d(1, 1)] = CELL::Alive;
-	grid[idx2dto1d(2, 2)] = CELL::Alive;
-	grid[idx2dto1d(2, 3)] = CELL::Alive;
-	grid[idx2dto1d(1, 3)] = CELL::Alive;
-	grid[idx2dto1d(0, 3)] = CELL::Alive;
-
 	let sdl_context = sdl2::init().unwrap();
 	let video_subsystem = sdl_context.video().unwrap();
 
@@ -129,8 +119,16 @@ fn main() {
 
 	let mut canvas = window.into_canvas().build().unwrap();
 	let mut event_pump = sdl_context.event_pump().unwrap();
+
+	let mut grid = Grid::new();
+	grid.add_cell((1, 1), CellType::Alive);
+	grid.add_cell((2, 2), CellType::Alive);
+	grid.add_cell((2, 3), CellType::Alive);
+	grid.add_cell((1, 3), CellType::Alive);
+	grid.add_cell((0, 3), CellType::Alive);
+
 	'running: loop {
-		canvas.set_draw_color(Color::RGB(0, 0, 0));
+		canvas.set_draw_color(Color::RGB(100, 100, 100));
 		canvas.clear();
 		for event in event_pump.poll_iter() {
 			match event {
@@ -143,9 +141,56 @@ fn main() {
 			}
 		}
 
-		grid = game_cyle(&grid, &mut canvas);
+		game_cyle(&mut grid, &mut canvas);
 
 		canvas.present();
 		sleep(Duration::from_millis(500));
 	}
+}
+const QUAD_WIDTH: u32 = 20;
+const QUAD_HEIGHT: u32 = 20;
+
+fn game_cyle(grid: &mut Grid, canvas: &mut Canvas<sdl2::video::Window>) {
+	let mut new_grid = Grid::new();
+
+	for (k, v) in &grid.grid {
+		let neigh = grid.get_neigh_count(k.0, k.1);
+		match v.cell_type {
+			CellType::Alive => {
+				if neigh < 2 {
+					new_grid.add_cell(*k, CellType::Dead);
+				} else if neigh == 2 || neigh == 3 {
+					new_grid.add_cell(*k, CellType::Alive);
+				} else if neigh >= 3 {
+					new_grid.add_cell(*k, CellType::Dead);
+				}
+			}
+			CellType::Dead => {
+				if neigh == 3 {
+					new_grid.add_cell(*k, CellType::Alive);
+				}
+			}
+		}
+
+		match new_grid.grid.get(&k) {
+			Some(c) => {
+				if c.cell_type == CellType::Alive {
+					canvas.set_draw_color(Color::RGB(255, 255, 255));
+				} else {
+					canvas.set_draw_color(Color::RGB(0, 0, 0));
+				}
+			}
+			None => {}
+		}
+
+		let rectangle = sdl2::rect::Rect::new(
+			k.0 as i32 * (1 + QUAD_WIDTH as i32) + GAP as i32,
+			k.1 as i32 * (1 + QUAD_HEIGHT as i32) + GAP as i32,
+			QUAD_WIDTH,
+			QUAD_HEIGHT,
+		);
+		let _ = canvas.fill_rect(rectangle);
+	}
+
+	*grid = new_grid;
 }
